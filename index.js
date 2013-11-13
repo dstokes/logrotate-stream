bd75ba937d07551b0d7337c54587990e953e4d3b
Drew Stokes <drew@drewstokes.com>
2013-11-13 09:22:50 -0800

    improve queueing logic, fix fd error, simplify code

--- index.js
+++ index.js
@@ -11,57 +11,52 @@ function LogStream(options) {
   if (! (this instanceof LogStream)) { return new LogStream(options); }
   Writable.call(this, options);
 
-  var self = this;
   this.file = options.file;
-  this.size = (options.size || 200);
-  this.keep = (options.keep || 3);
-  this.compress = (options.compress || false);
+  this.size = (options.size || 51200000000 /* 50MB */);
+  this.rotateOptions = {
+    count: (options.keep || 3),
+    compress: (options.compress || false)
+  };
 
-  this._openWriteStream();
+  this._createWriteStream();
 }
 
-LogStream.prototype._openWriteStream = function() {
+LogStream.prototype._createWriteStream = function() {
   var self = this;
-  this.log = fs.createWriteStream(this.file, { flags: 'a+' });
+  this.writer = fs.createWriteStream(this.file, { flags: 'a+' });
 
-  this.log.once('open', function() {
+  this.writer.once('open', function() {
     fs.stat(self.file, function(err, st) {
-      // set the file size to 0 if stat fails
-      self.log.size = err ? 0 : st.size;
-      self.emit('ready', self.log);
+      if (err) return self.emit('error', err);
+      self.writer.size = st.size;
+      self.emit('ready', self.writer);
     });
   });
 }
 
-LogStream.prototype.queue = function(chunk, encoding) {
-  if(this.log.destroyed !== true) {
-    this.log.write(chunk, encoding);
-    this.log.size += chunk.length;
-  } else {
-    this.once('ready', function(log) {
-      log.write(chunk, encoding);
-      log.size += chunk.length;
+LogStream.prototype._write = function(chunk, encoding, cb) {
+  var rotate = this.writer.size > this.size;
+
+  if (rotate || typeof this.writer.size === 'undefined') {
+    if (rotate) this._rotate()
+    return this.once('ready', function() {
+      this.writer.write(chunk, encoding)
+      this.writer.size += chunk.length;
+      cb();
     });
   }
-}
 
-LogStream.prototype._write = function(chunk, encoding, cb) {
-  // rotate if the log file has grown too large
-  if(this.log.size > this.size) {
-    this._rotate();
-  }
-
-  this.queue(chunk, encoding);
-  cb()
+  this.writer.write(chunk, encoding)
+  this.writer.size += chunk.length;
+  cb();
 }
 
 LogStream.prototype._rotate = function() {
-  var self = this
-    , options = { count: this.keep, compress: this.compress };
-
   // destroy the current log stream
-  this.log.removeAllListeners();
-  this.log.destroy();
+  this.writer.end();
 
-  rotate(this.file, options, function(err) { self._openWriteStream(); });
+  rotate(this.file, this.rotateOptions, function(err) {
+    if (err) return this.emit('error', err);
+    this._createWriteStream();
+  }.bind(this));
 }
