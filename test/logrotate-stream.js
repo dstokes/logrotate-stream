d0fd5abf7fd113f34b4c28364532ac0300a02156
Drew Stokes <drew@drewstokes.com>
2013-11-13 17:06:00 -0800

    add test for kept logs

--- test/logrotate-stream.js
+++ test/logrotate-stream.js
@@ -1,16 +1,30 @@
 var fs = require('fs')
   , test = require('tape')
+  , path = require('path')
   , logRotate = require('../');
 
+function fileName() { return __dirname +"/test.log_"+ Date.now(); }
+function writes(s, ct) {
+  var done = ct || 100
+    , inv;
+
+  inv = setInterval(function() {
+    var chunk = Date.now() + "\n"; // 14 bytes
+    s.write(chunk);
+    if (! --done) {
+      clearInterval(inv);
+      s.end();
+    }
+  }, 1);
+}
+
 test('rotates based on file size option', function(t) {
-  var file = __dirname+"/test.log" +Date.now();
+  var files = []
+    , file = fileName();
+
   var s = logRotate({ file: file, size: 600 });
 
-  var bytes = 0;
-  var done = 100;
-  var files = [];
-
-  s.on('finish',function() {
+  s.on('finish', function() {
     t.equals(files.length, 2, 'should have 2 files');
     // TODO clean up files
     t.end();
@@ -20,15 +34,28 @@ test('rotates based on file size option', function(t) {
     files.push(file);
   });
 
-  var data;
-  data = setInterval(function() {
-    var chunk = Date.now() +"\n";// 14 bytes
-    s.write(chunk);
-    bytes += chunk.length;
+  // start writing
+  writes(s, 100);
+});
 
-    if (!--done) {
-      clearInterval(data);
-      s.end();
-    }
-  }, 1);
+test('keeps the appropriate number of rotated logs', function(t) {
+  var file = fileName()
+    , s = logRotate({ file: file, size: 600, keep: 5 });
+
+  s.on('finish', function() {
+    console.log('finished');
+    fs.readdir(__dirname, function(err, files) {
+      var kept = 0;
+      for (var i = 0, l = files.length; i < l; i++) {
+        if (files[i].indexOf(path.basename(file)) !== -1) kept++;
+      }
+
+      t.equals(kept, 5, 'should keep 5 files');
+      // TODO clean up files
+      t.end();
+    });
+  })
+
+  // start writing
+  writes(s, 200);
 });
